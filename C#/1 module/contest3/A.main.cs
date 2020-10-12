0da5efdf6f84f9ad030e85aa90c04c260f3acc05
ShipilA <44925533+ShipilA@users.noreply.github.com>
2020-10-12 21:07:05 +0300

    Create A.main.cs

---
+++ C#/1 module/contest3/A.main.cs
@@ -0,0 +1,20 @@
+  
+using System;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        if (!TryParseInput(Console.ReadLine(), Console.ReadLine(), out int a, out int b))
+        {
+            Console.WriteLine("Incorrect input");
+        }
+        else
+        {
+            SwapMaxDigit(ref a, ref b);
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
