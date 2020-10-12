8eb138d19451664e280874240d1187194e576e9d
ShipilA <44925533+ShipilA@users.noreply.github.com>
2020-10-12 21:01:05 +0300

    Create A.cs

---
+++ C#/1 module/contest1/A.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            Console.WriteLine(s);
+            Console.WriteLine(s);
+            Console.WriteLine(s);
+        }
+    }
+}
